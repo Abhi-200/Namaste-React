f00eb1269f96b7a102f274b1bfb0e4860f48be1e
Abhi-200 <abhisheklaxkar125@gmail.com>
2023-12-14 18:04:53 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
+      React.createElement("h1", {}, "hello guys are made"),
+      React.createElement("h2", {}, "your are pagal ladki"),
     ]),
     React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
+      React.createElement("h1", {}, "your are pagal bhendi ladki"),
+      React.createElement("h2", {}, "your are pagal bhendi ladki"),
     ]),
   ]);
   
---
+++ README.md
@@ -0,0 +1,18 @@
+# parcel
+- Dev build
+- Local server
+- HMR = Hot module replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Dagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
