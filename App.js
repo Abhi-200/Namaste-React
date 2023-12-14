a30666d99323ffa3f701a187e6aa33be1800ad50
Abhi-200 <abhisheklaxkar125@gmail.com>
2023-12-14 18:28:12 +0530

    new code

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "hello guys are made"),
-      React.createElement("h2", {}, "your are pagal ladki"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "your are pagal bhendi ladki"),
-      React.createElement("h2", {}, "your are pagal bhendi ladki"),
-    ]),
-  ]);
+const heading = React.createElement(
+  "h1",
+  {id: "heading"},
+  "Namaste React"
+  );
   
-  console.log(parent);
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
